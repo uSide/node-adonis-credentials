4695d2bd86fc56d22bf6308b7b2c9c0df001948b
Vladimir Kleshko <uside.node@gmail.com>
2018-09-27 23:11:05 +0300

    Get the value at path of object

--- src/Credentials.js
+++ src/Credentials.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const lodash = require("lodash");
 const yaml = require("js-yaml");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +11,30 @@ class Credentials {
     this.Encryption = new Encryption.constructor(
       Config.get("app.credentialsKey")
     );
+    this._credentials = {};
+
+    if (
+      !this.Helpers.isAceCommand() &&
+      process.argv.join().indexOf("credentials:") == -1
+    ) {
+      let files = fs
+        .readdirSync(path.join(this.Helpers.appRoot(), "config/credentials"))
+        .filter(e => e.indexOf(".enc") > -1)
+        .map(e => e.split(".")[0]);
+
+      for (let filename of files) {
+        let encrypted = fs.readFileSync(
+          path.join(
+            this.Helpers.appRoot(),
+            "config/credentials",
+            `${filename}.enc`
+          ),
+          "utf8"
+        );
+
+        this._credentials[filename] = yaml.safeLoad(this.decrypt(encrypted));
+      }
+    }
   }
 
   encrypt(data) {
@@ -20,24 +45,8 @@ class Credentials {
     return this.Encryption.decrypt(data);
   }
 
-  get(filename) {
-    if (
-      this.Helpers.isAceCommand() &&
-      process.argv.join().indexOf("credentials:") > -1
-    ) {
-      return {};
-    }
-
-    let encrypted = fs.readFileSync(
-      path.join(
-        this.Helpers.appRoot(),
-        "config/credentials",
-        `${filename}.enc`
-      ),
-      "utf8"
-    );
-
-    return yaml.safeLoad(this.decrypt(encrypted));
+  get(key, defaultValue) {
+    lodash.get(this._credentials, key, defaultValue);
   }
 }
 
